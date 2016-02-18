707e6e8ca80181110d383416e059f80a73f86ea4
kayneth <dylan.temboucti@gmail.com>
2016-02-18 10:46:06 +0100

    modif style

---
+++ dist/inscri.php
@@ -0,0 +1,26 @@
+<?php
+	include('header.php');
+	include('nav.php');
+
+?>
+	<section id="content">
+		<div id="progra" class="container">
+			<div class="row">
+				<h2 class="col-md-11">Programme</h2>
+			</div>
+			<div id="progra-contain-glob">
+				<div class="row">
+					<h4>Inscription</h4>
+					<p>Vous pouvez choisir plusieurs ateliers sur la journée. Veillez cependant à choisir des crénaux dont les horaires ne se chevauchent pas. Vous ne pouvez vous inscrire deux fois à un même atelier dans la journée. Nous comptons sur votre présence si vous vous inscrivez, veuillez nous prévenir en cas de desistement ou d’empêchementà l’adresse fanmmi.castres@gmail.com.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script type="text/javascript">
+		var active = "programme";
+	</script>
+
+<?php
+	include('footer.php');
+?>
